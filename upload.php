35bcdf52f06a325d08dd15b4620ef9fa6a9fcbb3
SzbLaszlo <92303851+SzbLaszlo@users.noreply.github.com>
2021-12-02 10:14:00 +0100

    friss

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ echo $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
 
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file has been uploaded.";
+    echo " The file has been uploaded.";
   }
 }
 
